9c6484adbe172a5ba2d32d97a852ed3eeb7796c2
Pierre-Alexandre St-Jean <pastjean@pastjean.local>
2015-10-27 14:58:08 -0400

    Some basic working client

--- search/client.go
+++ search/client.go
@@ -1,15 +1,66 @@
 package search
 
-type Client struct {
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Client interface {
+	Query(q Query) (*Response, error)
 }
 
 type Config struct {
-	Token string
+	Token     string
+	UserAgent string
 }
 
-func NewClient(c Config) (*Client, error) {
-	return &client, nil
+func NewClient(c Config) (Client, error) {
+	return &client{
+		token:      c.Token,
+		endpoint:   "https://cloudplatform.coveo.com/rest/search/",
+		httpClient: http.DefaultClient,
+		useragent:  c.UserAgent,
+	}, nil
 }
 
-func (c *Client) Query(q Query) (*Response, error) {
+type client struct {
+	httpClient *http.Client
+	token      string
+	endpoint   string
+	useragent  string
+}
+
+func (c *client) Query(q Query) (*Response, error) {
+	marshalledQuery, err := json.Marshal(q)
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewReader(marshalledQuery)
+
+	req, err := http.NewRequest("POST", c.endpoint, buf)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+c.token)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accepts", "application/json")
+	req.Header.Set("User-Agent", c.useragent)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusOK {
+		err := fmt.Errorf("Some error with the request")
+		return nil, err
+	}
+
+	queryResponse := &Response{}
+	err = json.NewDecoder(resp.Body).Decode(queryResponse)
+	return queryResponse, err
 }
