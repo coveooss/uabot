bb97628c4351f49ce3b54d6b606138787461f140
Etienne Rocheleau <erocheleau@coveo.com>
2016-06-07 14:43:18 -0400

    Delete localdebug.sh

--- localdebug.sh
+++
@@ -1,6 +0,0 @@
-export SEARCHTOKEN=575f52d3-4d29-46df-ac94-e5ebf6720d2b
-export UATOKEN=575f52d3-4d29-46df-ac94-e5ebf6720d2b
-export SCENARIOSURL=Scenarios\\BTScenarios.json
-export LOCAL=true #if scenariosurl is a local path
-export GO15VENDOREXPERIMENT=1 #if golang version < 1.6
-go run main.go
