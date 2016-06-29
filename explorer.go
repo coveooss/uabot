b61fdcd2c7447366b424187dfa2fb5381db30244
Adam B Bolduc <abolduc@coveo.com>
2016-06-29 13:58:54 -0400

    Refactored Loggers Refactored Scenario input

--- explorer.go
+++ explorer.go
@@ -1,32 +1,27 @@
 package main
 
 import (
-	"log"
 	"os"
 	"github.com/adambbolduc/uabot/explorerlib"
 	"github.com/erocheleau/uabot/scenariolib"
 	"encoding/json"
 	"io/ioutil"
 	"flag"
+	"time"
+	"math/rand"
 )
-
-var (
-	Info *log.Logger
-)
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 var (
 	configFile = flag.String("config", "config.json", "File Path for configuration File")
 )
 
 func main() {
-	Info = log.New(os.Stdout, "INFO | ", log.Ldate | log.Ltime)
+	scenariolib.InitLogger(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
 
+	source := rand.NewSource(int64(time.Now().Unix()))
+	random := rand.New(source)
+
+	scenariolib.Info.Println("Reading config file")
 	var config explorerlib.Config
 	bytes, readErr := ioutil.ReadFile(*configFile)
 	check(readErr)
@@ -45,59 +40,22 @@ func main() {
 	languages, status := index.FetchLanguages();
 	check(status)
 
-	err := explorerlib.NewScenarioBuilder().WithOrgName(config.Org).WithSearchEndpoint(config.SearchEndpoint).WithAnalyticsEndpoint(config.AnalyticsEndpoint).AllAnonymous().WithLanguages(languages).WithWordCountsByLanguage(wordCountsByLanguage, config.NumberOfQueryByLanguage, config.AverageNumberOfWordsPerQuery).WithTimeBetweenActions(1).WithTimeBetweenVisits(5).WithScenarios(&scenariolib.Scenario{
-		Name: "scenario1",
-		Events:[]scenariolib.JSONEvent{
-			scenariolib.JSONEvent{
-				Type:"Search",
-				Arguments: map[string]interface{}{
-					"queryText" : "",
-					"goodQuery" : true,
-					"matchLanguage":true,
-				},
-			},
-			scenariolib.JSONEvent{
-				Type:"Click",
-				Arguments: map[string]interface{}{
-					"docNo": -1,
-					"offset": 0,
-					"probability": 0.3,
-				},
-			},
-			scenariolib.JSONEvent{
-				Type:"View",
-				Arguments: map[string]interface{}{
-					"docNo": -1,
-					"offset": 0,
-					"probability": 0.3,
-				},
-			},
-			scenariolib.JSONEvent{
-				Type: "Search",
-				Arguments: map[string]interface{}{
-					"queryText" :"",
-					"goodQuery":true,
-					"matchLanguage":true,
-				},
-			},
-			scenariolib.JSONEvent{
-				Type:"Click",
-				Arguments: map[string]interface{}{
-					"docNo": 1,
-					"offset": 0,
-					"probability": 0.5,
-				},
-			},
-			scenariolib.JSONEvent{
-				Type:"View",
-				Arguments: map[string]interface{}{
-					"docNo": 1,
-					"offset": 0,
-					"probability": 0.5,
-				},
-			},
-		},
-		Weight: 1,
-	}).Save(config.OutputFilePath)
+	err := explorerlib.NewScenarioBuilder().WithOrgName(config.Org).WithSearchEndpoint(config.SearchEndpoint).WithAnalyticsEndpoint(config.AnalyticsEndpoint).AllAnonymous().WithLanguages(languages).WithWordCountsByLanguage(wordCountsByLanguage, config.NumberOfQueryByLanguage, config.AverageNumberOfWordsPerQuery).WithTimeBetweenActions(1).WithTimeBetweenVisits(5).WithScenarios(config.MainScenario).Save(config.OutputFilePath)
+	check(err)
+
+	scenariolib.Info.Println("Running Bot")
+
+	conf, err := scenariolib.NewConfigFromPath(config.OutputFilePath)
+	check(err)
+
+	uabot := scenariolib.NewUabot(true, conf, config.OutputFilePath, config.SearchToken, config.AnalyticsToken, random)
+	err =uabot.Run()
 	check(err)
 }
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
