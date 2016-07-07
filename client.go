17eccc6acece97ab3a854d7b83f89110cf0a660f
Adam B Bolduc <abolduc@coveo.com>
2016-07-07 14:00:50 -0400

    Added threadpool

--- client.go
+++ client.go
@@ -4,9 +4,17 @@ import (
 	"github.com/adambbolduc/uabot/server"
 	"log"
 	"net/http"
+	"github.com/goinggo/workpool"
+	"runtime"
+)
+
+var(
+	workPool *workpool.WorkPool
 )
 
 func main() {
+	workPool = workpool.New(runtime.NumCPU(), 100)
+	server.Inject(workPool)
 	router := server.NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
